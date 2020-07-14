082d072048a4783e590ff53c3578662dc003fed2
Willy <willy@correomodificado.com>
2020-07-14 17:03:23 -0500

    Subida inicial

---
+++ CSharp-y-Nodejs/FechaActual_CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpVSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaHoy= DateTime.Now;
+            Console.WriteLine(fechaHoy.ToString("dd/MM/yyyy"));
+            
+        }
+    }
+}
---
+++ CSharp-y-Nodejs/FechaActual_Nodejs/app.js
@@ -0,0 +1,4 @@
+var moment = require('moment');
+
+var fechaHoy= moment(new Date()).format("DD/MM/yyyy");
+console.log(fechaHoy);
