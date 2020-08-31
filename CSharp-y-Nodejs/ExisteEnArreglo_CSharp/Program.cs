e93111a96dd5b7f478b0b4cfe375d7eab8011ce9
Willy <willy@correomodificado.com>
2020-08-31 18:10:32 -0500

    mod existe en arreglo

---
+++ CSharp-y-Nodejs/ExisteEnArreglo_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExisteEnArreglo_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verificar si existe "Amarillo" en el arreglo
+            string[] cadArray= new string[]{"Blanco","Negro","Verde","Azul"};
+            bool existe=Array.Exists(cadArray, item=>item=="Amarillo");
+            Console.WriteLine(existe);
+        }
+    }
+}
---
+++ CSharp-y-Nodejs/ExisteEnArreglo_Nodejs/app.js
@@ -0,0 +1,6 @@
+//Verificar si existe "Amarillo" en el arreglo
+var cadArray=["Blanco","Negro","Verde","Azul"];
+
+var existe=cadArray.includes("Amarillo");
+
+console.log(existe);
